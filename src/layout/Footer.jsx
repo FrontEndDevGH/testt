7d723af688124555913e7608c04c48ae38fc9c7b
Farid <faleskerov@gid.ru>
2023-07-01 20:04:01 +0400

    first commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer blue accent-4">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright Text
+      <a className="grey-text text-lighten-4 right" href="https://github.com/Arnohollowknight/Movie-project">Repo</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export {Footer}
